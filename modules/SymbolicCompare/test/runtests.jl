39da22e4401b3091d4860c93e92c4c2eda64d4da
Ritchie Lee <ritchie.lee@sv.cmu.edu>
2017-04-17 10:28:00 -0700

    fix SymbolicCompare test

--- modules/SymbolicCompare/test/runtests.jl
+++ modules/SymbolicCompare/test/runtests.jl
@@ -32,7 +32,6 @@
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 # *****************************************************************************
 
-using GrammarExpts
-using SymbolicCompare
+using ExprSearch, SymbolicCompare
 
 
