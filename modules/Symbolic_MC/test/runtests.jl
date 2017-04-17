e51f0f220aa60002df694a7fe09f5a96c450360e
Ritchie Lee <ritchie.lee@sv.cmu.edu>
2017-04-17 10:35:43 -0700

    fix symbolic tests

--- modules/Symbolic_MC/test/runtests.jl
+++ modules/Symbolic_MC/test/runtests.jl
@@ -32,7 +32,7 @@
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 # *****************************************************************************
 
-using GrammarExpts
+using ExprSearch
 using Symbolic_MC
 
 
